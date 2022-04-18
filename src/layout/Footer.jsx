2ad61f332b485bf708fcf8449d991d95bf2ab45d
Dmitriy Dudnik <ddudnik79@gmail.com>
2022-04-18 13:07:30 +0300

    base commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <footer className="page-footer green darken-1">
+            <div className="container"></div>
+            <div className="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Text
+                    <a className="grey-text text-lighten-4 right" href="#!">
+                        Repo
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+export { Footer };
