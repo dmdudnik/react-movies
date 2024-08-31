3cf445fef9f8aad36590da923206038e20bf3f66
Dmitriy Dudnik <63814431+dmdudnik@users.noreply.github.com>
2024-08-31 18:18:00 +0300

    Add Movie component

---
+++ src/components/Movie.jsx
@@ -0,0 +1,23 @@
+function Movie (props){
+    const{
+        Title: title,
+        Year: year,
+        imdbId: id,
+        Type: type,
+        Poster: poster
+    } = props
+
+    return(
+        <div id ={id} className="movie card">
+    <div className="card-image waves-effect waves-block waves-light">
+      <img className="poster" src={poster}/>
+    </div>
+    <div className="card-content">
+      <span className="card-title activator grey-text text-darken-4">{title}</span>
+      <p>{type}<span>{year}</span></p>
+    </div>
+  </div>
+    )
+
+}
+export {Movie}
---
+++ src/components/Movies.jsx
@@ -0,0 +1,4 @@
+function Movies () {
+
+}
+export {Movies}
