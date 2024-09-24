617a47c18adb6b7f4382480d84314f186e19163b
Dmitriy Dudnik <ddudnik79@gmail.com>
2024-09-24 17:23:48 +0300

    Add wrong search

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,15 +1,16 @@
-import Main from '../layout/Main'
-import { Movie } from './Movie'
+import { Movie } from './Movie';
 
 function Movies(props) {
-    const { movies } = props
+    const { movies = [] } = props;
 
     return (
-        <div className='movies'>
-            {movies.map(movie => (
-                <Movie key={movie.imdbID}{...movie} />
-            ))}
+        <div className="movies">
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <h4>Nothing found</h4>
+            )}
         </div>
-    )
+    );
 }
-export { Movies }
+export { Movies };
