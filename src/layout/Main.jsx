252eeb7e1575e63150e480e5773806c77f07998f
Dmitriy Dudnik <ddudnik79@gmail.com>
2024-09-25 17:36:01 +0300

    add HTTPS protocol

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -11,7 +11,7 @@ class Main extends React.Component {
     };
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=tt3896198&s=matrix`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=tt3896198&s=matrix`)
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
@@ -21,7 +21,7 @@ class Main extends React.Component {
     searchMovies = (str, type = 'all') => {
         this.setState({ loading: true });
         fetch(
-            `http://www.omdbapi.com/?apikey=${API_KEY}&i=tt3896198&s=${str}${
+            `https://www.omdbapi.com/?apikey=${API_KEY}&i=tt3896198&s=${str}${
                 type !== 'all' ? `&type=${type}` : ''
             }`
         )
